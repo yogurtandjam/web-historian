4e721b689906294198b68af7a70c970152b9d430
Hack Reactor Student <student@hackreactor.com>
2018-05-17 20:01:03 -0700

    Mostly Done.

--- web/request-handler.js
+++ web/request-handler.js
@@ -2,6 +2,7 @@ var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var httpHelpers = require('../web/http-helpers');
 var fs = require('fs');
+var http = require('http');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
@@ -14,6 +15,15 @@ exports.handleRequest = function (req, res) {
     if (req.url.match('styles.css')) {
       httpHelpers.serveAssets(res, archive.paths.siteAssets + '/styles.css', fs.readFile);
     }
+    else {
+      // let url = req.url.slice(4);
+      console.log(req.url)
+      archive.isUrlArchived(req.url, function(exists) {
+        if (exists) {
+          httpHelpers.serveAssets(res, url, fs.readFile);
+        }
+      })
+    }
   }
 
   if (req.method === 'POST') {
@@ -23,13 +33,36 @@ exports.handleRequest = function (req, res) {
     res.writeHead(statusCode, httpHelpers.headers)
     req.on('data', (chunk) => {
       body += chunk;
-      console.log('tHISSSS ISSSS BODY YYYYYYYYYYYYYY ',body)
     })
     req.on('end', ()=> {
       let url = body.slice(4);
+      console.log(url)
       archive.isUrlArchived(url, function(exists){
         if (exists) {
           httpHelpers.serveAssets(res, url, fs.readFile);
+        } else {
+          archive.isUrlInList(url, function(exists, err) {
+            if (exists) {
+              console.log('url is in the list', url)
+              http.get('http://www.youtube.com/', (res) => {
+                // if(res.statusCode !== 200){console.log('error')}
+                res.setEncoding('utf8');
+                let rawData = '';
+                res.on('data', (chunk) => {rawData += chunk})
+                console.log(rawData)
+                if(rawData) {
+                    archive.downloadUrls(url, rawData);
+                  }
+              })
+            } else {
+              archive.addUrlToList(url, function() {
+                archive.isUrlInList(url, function(exists) {
+                  if (exists) {console.log('url is now in list')}
+                  else {console.log('something went wrong!')}
+                })
+              })
+            }
+          })
         }
       })
     })
